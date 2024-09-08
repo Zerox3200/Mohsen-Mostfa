63a6e381fb040eb2f1b877881b545d2023f77be5
Zerox3200 <ziad.ahmed01020338066@gmail.com>
2024-09-08 19:51:26 +0300

    Accessability updates

--- src/Components/HomeSections/Footer/Footer.jsx
+++ src/Components/HomeSections/Footer/Footer.jsx
@@ -10,16 +10,16 @@ export default function Footer() {
         © 2024 Mohsen Mostfa. All rights reserved
         <span>Designed By  A to Z tech </span>
         <div className="d-flex gap-2 justify-content-center align-content-center mt-3">
-            <a href="https://wa.me/+201025565499" target='_blank' rel="noreferrer">
+            <a href="https://wa.me/+201025565499" target='_blank' rel="noreferrer" aria-label="What's up">
                 <FaWhatsapp />
             </a>
-            <a href="https://www.linkedin.com/in/mohsen-mostafa-20526718b" target='_blank' rel="noreferrer">
+            <a href="https://www.linkedin.com/in/mohsen-mostafa-20526718b" target='_blank' rel="noreferrer" aria-label="Linked in">
                 <FaLinkedin />
             </a>
-            <a href="https://www.kaggle.com/babydriver1233" target='_blank' rel="noreferrer">
+            <a href="https://www.kaggle.com/babydriver1233" target='_blank' rel="noreferrer" aria-label="Kaggel">
                 <FaKaggle />
             </a>
-            <a href="https://medium.com/@mohsenmostafa833" target='_blank' rel="noreferrer">
+            <a href="https://medium.com/@mohsenmostafa833" target='_blank' rel="noreferrer" aria-label="Meduim">
                 <FaMediumM />
 
             </a>
